4421fefeb204e541b98098bf9c2a9e5c661739fa
brodie02 <marshallbrodie0@gmail.com>
2022-11-26 14:32:23 +1030

    Add update employee role function

--- db/query.sql
+++ db/query.sql
@@ -2,14 +2,16 @@
 -- FROM employee
 -- JOIN role ON employee.role_id = role.id;
 
--- Roles
 -- SELECT role.id, title, name, salary
 -- FROM role
 -- JOIN department ON role.department_id = department.id;
 
--- Employees
 -- SELECT employee_1.id, employee_1.first_name, employee_1.last_name, title, name as department, salary, CONCAT(employee.first_name, SPACE(1), employee.last_name) AS manager
 -- FROM employee as employee_1
 -- JOIN role ON employee_1.role_id = role.id
 -- JOIN department ON role.department_id = department.id
 -- LEFT OUTER JOIN employee ON employee_1.manager_id = employee.id;
+
+UPDATE employee
+SET role_id = 2
+WHERE id = 4
