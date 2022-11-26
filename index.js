106709d514e2750baa3beb348d4480738595e5cf
brodie02 <marshallbrodie0@gmail.com>
2022-11-26 15:28:25 +1030

    Add starting questions

---
+++ index.js
@@ -0,0 +1,57 @@
+const mysql = require('mysql2')
+const inquirer = require('inquirer')
+
+const {viewDepartments, viewRoles, viewEmployees} = require('./lib/view')
+const {addDepartment, addRole, addEmployee} = require('./lib/create')
+const {updateEmployeeRole} = require('./lib/update')
+
+const start = async () => {
+    const questions = [
+        {
+            type: 'list',
+            message: `What would you like to do?`,
+            name: 'choice',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        },
+    ]
+
+    const check = (answer) => {
+        switch (answer) {
+            case 'View All Employees':
+                return viewEmployees()
+
+            case 'Add Employee':
+                return addEmployee()
+
+            case 'Update Employee Role':
+                return updateEmployeeRole()
+
+            case 'View All Roles':
+                return viewRoles()
+
+            case 'Add Role':
+                return addRole()
+
+            case 'View All Departments':
+                return viewDepartments() 
+                
+            case 'Add Department':
+                return addDepartment()  
+        
+            default:
+                return console.log("Something went wrong!");
+        }
+    }
+
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            console.log(check(data.choice));
+        })
+}
+
+start()
+
+module.exports = {
+    start
+}
